22058b8e491a5545f537328ec26967aa4cc3c1a1
rex69420 <65942753+rex69420@users.noreply.github.com>
2023-01-01 01:32:12 +0530

    :D

--- script.js
+++
@@ -1,9 +0,0 @@
-const fancyText = document.querySelector('#fancy-text');
-
-fancyText.addEventListener('mouseover', () => {
-  fancyText.classList.add('hover');
-});
-
-fancyText.addEventListener('mouseout', () => {
-  fancyText.classList.remove('hover');
-});
